c6ef60efbd1cf9f11e78e711ea8477b68bf5c749
Pablo <percara.pablo@gmail.com>
2023-01-19 11:25:24 -0300

    nueva funcionalidad

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,30 @@
 const contenedor = document.querySelector('#contenedor')
 const entrada = document.querySelector('#entrada');
 let lista = document.querySelector('#lista');
-
-
 contenedor.addEventListener("submit", agregar);
+
 
 function agregar(e){
     e.preventDefault();
     if(entrada.value!=''){
-    let item = document.createElement('div')
-    let compra = entrada.value;
-    
-    item.innerHTML = `<div class="alert alert-light text-dark alert-dismissible fade show sombra">
-                    <strong>${compra}</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
-                     </div>`
-    
-    lista.append(item)
-    contenedor.reset();
-    }
-    else{
+        let compra=entrada.value
+        let item = document.createElement('div')
+        item.innerHTML = `<div class="alert alert-light text-dark alert-dismissible fade show sombra">
+                        <strong>${compra}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+                        </div>`
+            
+        lista.append(item)  
+        contenedor.reset();
+    }else{
         alert('Debe ingresar al menos un elemento')
     }
-
 }
+
+
+
+
+
 
     
 
