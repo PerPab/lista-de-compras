a4d6dc059921b3cfe3c2a62d9be530cafc063ad9
Pablo <percara.pablo@gmail.com>
2023-01-19 16:46:32 -0300

    localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+window.addEventListener('load', function() {
 
 
 const contenedor = document.querySelector('#contenedor')
@@ -6,6 +7,25 @@ const entrada = document.querySelector('#entrada');
 let lista = document.querySelector('#lista');
 contenedor.addEventListener("submit", agregar);
 
+
+let listaGuardada=[]
+let listaRecuperada = []
+
+function cargarLista(){
+    let datosRecuperados = localStorage.getItem('lista')
+    if(datosRecuperados!=''){
+    listaRecuperada = datosRecuperados.split(",")
+    for(let dato of listaRecuperada){
+        let item = document.createElement('div')
+        item.innerHTML = `<div class="alert alert-light text-dark alert-dismissible fade show sombra">
+                        <strong>${dato}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+                        </div>`
+            
+        lista.append(item) 
+    }
+}
+}
 
 function agregar(e){
     e.preventDefault();
@@ -15,14 +35,20 @@ function agregar(e){
         item.innerHTML = `<div class="alert alert-light text-dark alert-dismissible fade show sombra">
                         <strong>${compra}</strong>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
-                        </div>`
-            
-        lista.append(item)  
+                        </div>` 
+        lista.append(item); 
         contenedor.reset();
+        listaGuardada.push(compra)
+        localStorage.setItem('lista', listaGuardada) 
     }else{
         alert('Debe ingresar al menos un elemento')
     }
+   entrada.focus()
 }
+
+cargarLista()
+
+});
 
 
 
